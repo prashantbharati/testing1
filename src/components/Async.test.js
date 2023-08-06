3817dc062dd34049acf29465a4c1b2916f174199
Prashant  Bharati <pbharati@chegg.com>
2023-08-06 08:51:31 +0530

    mock functions

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,13 @@ import Async from "./Async";
 
 describe("testing the async funtions", () => {
   test("this is for the corret rendering iof the test functions", async () => {
+    // first mock function
+
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 1, post: "mock stuff" }],
+    });
+
     render(<Async />);
 
     const element = await screen.findAllByRole("listitem");
