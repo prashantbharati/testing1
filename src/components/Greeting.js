7e0c9844c0a383f1a399ebc693b1b59b2aeecc63
Prashant  Bharati <pbharati@chegg.com>
2023-08-06 08:18:51 +0530

    this case solved but last case error not

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,24 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Greeting = () => {
+  const [completed, setCompleted] = useState(false);
 
-    const[completed,setCompleted]=useState(false);
+  const changecompleted = () => {
+    setCompleted(true);
+  };
 
-    const changecompleted=()=>{
-        setCompleted(true)
-    }
-    
   return (
     <div>
-        
-        {!completed && <p>Hey this is before case</p>}
-        {console.log(completed)}
-        {completed&&<p>This is changed case</p>}
-        
-        <button id='button' onClick={changecompleted}>click me change the state</button>
+      <p>Hey this is before case</p>
+      {console.log(completed)}
+      {completed && <p>This is changed case</p>}
 
+      <button onClick={changecompleted}>click me change the state</button>
     </div>
-  )
-}
+  );
+};
 
-export default Greeting
+export default Greeting;
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,49 +1,53 @@
-import { render, screen } from "@testing-library/react"
-import Greeting from "./Greeting"
-import userEvent from "@testing-library/user-event"
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
-describe("this is to check the custom build fuctions",()=>{
+describe("this is to check the custom build fuctions", () => {
+  test("chehckinfg my csutom build test function", () => {
+    // Arrange
 
-    
-    test("chehckinfg my csutom build test function",()=>{
+    render(<Greeting />);
 
-        // Arrange
-    
-        render(<Greeting/>)
-    
-        // Act
-        // .. nothing
-    
-        // Assert
-        const handlemycase= screen.getAllByText("Thank you")
-        expect(handlemycase).toBeInTheDocument();
-    
-    })
+    // Act
+    // .. nothing
 
-    test("to check the completed false case",()=>{
-        
-        render(<Greeting/>)
+    // Assert
+    const handlemycase = screen.getAllByText("Thank you");
+    expect(handlemycase).toBeInTheDocument();
+  });
 
-        const handlecase=screen.getByText('Hey this is before case',{exact:false})
-        expect(handlecase).toBeInTheDocument();
+  test("to check the completed false case", () => {
+    render(<Greeting />);
 
-    })
+    const handlecase = screen.getByText("Hey this is before case", {
+      exact: false,
+    });
+    expect(handlecase).toBeInTheDocument();
+  });
 
-    test('this is on the button click',()=>{
-        // Arrange
-        render(<Greeting/>)
+  test("this is on the button click", async () => {
+    // Arrange
 
-        // Act
-        const element=screen.getByRole("button")
-        userEvent.click(element)
-     
-        //Assert
-        const handlecase=screen.getByText('This is changed case')
-        expect(handlecase).toBeInTheDocument();
+    // await act(async () => render(<Greeting />));
+    render(<Greeting />);
 
-        
+    // Act
+    const element = screen.getByRole("button");
+    userEvent.click(element);
 
-    })
+    //Assert
+    const handlecase = screen.getByText("This is changed case");
+    expect(handlecase).toBeInTheDocument();
+  });
 
-})
+  test("this is 4th case for text remonval", async () => {
+    // await act(async () => render(<Greeting />));
+    render(<Greeting />);
 
+    const element = screen.getByRole("button");
+    userEvent.click(element);
+
+    const handlecase = screen.queryByText("Hey this is before case");
+    expect(handlecase).toBeNull();
+  });
+});
